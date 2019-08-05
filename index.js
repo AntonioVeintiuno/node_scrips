1dc2307e75126c6afe7c2a22b2b80b92e2983518
Antonio Garcia <moy_357@hotmail.com>
2019-08-05 00:35:30 -0600

    Remove credentials file.
    Signed-off-by: Antonio Garcia <moy_357@hotmail.com>

--- index.js
+++ index.js
@@ -71,51 +71,6 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the names and IDs of up to 10 files.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-// function listFiles(auth) {
-//   const drive = google.drive({version: 'v3', auth});
-//   drive.files.list({
-//     pageSize: 10,
-//     fields: 'nextPageToken, files(id, name)',
-//   }, (err, res) => {
-//     if (err) return console.log('The API returned an error: ' + err);
-//     const files = res.data.files;
-//     if (files.length) {
-//       console.log('Files:');
-//       files.map((file) => {
-//         console.log(`${file.name} (${file.id})`);
-//       });
-//     } else {
-//       console.log('No files found.');
-//     }
-//   });
-// }
-
-// function uploadFile(auth) {
-//   const drive = google.drive({version: 'v3', auth});
-//   var fileMetadata = {
-//     'name': 'My Report',
-//     'mimeType': 'application/vnd.google-apps.spreadsheet'
-//   };
-//   var media = {
-//     mimeType: 'text/xlsx',
-//     body: fs.createReadStream('demo.xlsx')
-//   };
-//   drive.files.create({
-//     resource: fileMetadata,
-//     media: media,
-//     fields: 'id'
-//   }, function (err, file) {
-//     if (err) {
-//        return console.log('The API returned an fucking error: ' + err);
-//         // Handle error
-//       // console.error(err);
-//     } else {
-//       console.log('File Id:', file.id);
-//     }
-//   });
-// }
-// application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
-// application/vnd.google-apps.spreadsheet
 function uploadFile(auth){
   var drive = google.drive('v3');
   var fileMetadata = {
@@ -126,7 +81,6 @@ function uploadFile(auth){
           //PATH OF THE FILE FROM YOUR COMPUTER
           body: fs.createReadStream('demo.xlsx')
       };
-
       drive.files.create({
           auth: auth,
           resource: fileMetadata,
@@ -137,11 +91,7 @@ function uploadFile(auth){
           // Handle error
           console.error(err);
       } else {
-          console.log('File Id: ', file.id);
+          console.log('File Id: ', file.data.id);
       }
    });
 }
-
-// codigo que se ingresa para accesar la  metadata
-// 4/lgHTMwaVt6DR9LxSQdanMGGJZfBJxXfstCroCqMRfUMbyc8kCUlVosM
-// 4/lgEIqnjwyFvc3V8N3IIeMb_zaKF1N84C-nYMEmrmuWKTI1QPLIYeezo
