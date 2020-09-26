ed9f2abc1ac5ff45504f348d5ca45417d7e9b887
AntonioVeintiuno <moy_357@hotmail.com>
2020-09-26 15:23:40 -0500

    Add example postgres db conection.
    Signed-off-by: AntonioVeintiuno <moy_357@hotmail.com>

---
+++ postgres/pgScript.js
@@ -0,0 +1,29 @@
+// this script read a file and update data in posgresdb
+const readXlsxFile = require('read-excel-file/node');
+const { Client } = require('pg');
+
+const client = new Client({
+  user: 'usuario',
+  host: 'localhost',
+  database: 'test',
+  password: '12345678',
+  port: 5432,
+})
+
+// File path.
+readXlsxFile('./routesBakcup.xlsx').then(async(rows) => {
+  client.connect();
+  rows.map(async (row, i ) => {
+    if (i > 0) {
+      let query = `UPDATE "Routes" SET "originAddress"='${row[3]}' WHERE "id"=${row[0]};`;
+      console.log('query', query);
+        try {
+          const res = await client.query(query);
+          console.log('Update is successfully', i);
+        } catch (err) {
+          console.log('error alv(amo la vida) ',i, err)
+            console.log(err.stack);
+        }
+    }
+  });
+})
